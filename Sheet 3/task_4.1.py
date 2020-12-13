c72be47fd5b60814a7cf683d46b99f80034a8492
velcorn <iwishyouperish@gmail.com>
2020-12-13 01:25:54 +0100

    It works, just not pretty :P

--- Sheet 3/task_4.1.py
+++ Sheet 3/task_4.1.py
@@ -1,33 +1,29 @@
-import numpy as np
-from scipy.optimize import linear_sum_assignment
+def calc_max_value(M):
+    max_value = 0
+    Z = [0, 1, 2, 3, 4, 5]
+    for p in permutations(Z):
+        val = 0
+        for i in range(len(M)):
+            val += M[i][p[i]]
+        if val > max_value:
+            max_value = val
+    return max_value
 
 
-def max_value(M):
-    M2 = subtract_means(M)
-    # print(M2)
-    Z = np.zeros(6)
-    rows = np.zeros(6)
-    cols = np.zeros(6)
-    while len(Z) < 6:
-        max_idx = np.where(M2 == np.max(M2))
-    return M2
+def permutations(lst):
+    if len(lst) <= 1:
+        yield lst
+    else:
+        for p in permutations(lst[1:]):
+            for i in range(len(lst)):
+                yield p[:i] + lst[0:1] + p[i:]
 
 
-def subtract_means(M):
-    M2 = np.copy(M)
-    M2 = M2 - np.mean(M2, axis=0) - np.mean(M2, axis=1) - np.mean(M2)
-    return M2
+M = [[8, 5, 17, 13, 9, 1],
+     [2, 5, 9, 1, 4, 6],
+     [12, 13, 15, 2, 3, 7],
+     [4, 2, 2, 4, 1, 4],
+     [11, 11, 5, 9, 4, 10],
+     [2, 7, 16, 9, 7, 5]]
 
-
-M = np.array([[8, 5, 17, 13, 9, 1],
-              [2, 5, 9, 1, 4, 6],
-              [12, 13, 15, 2, 3, 7],
-              [4, 2, 2, 4, 1, 4],
-              [11, 11, 5, 9, 4, 10],
-              [2, 7, 16, 9, 7, 5]])
-print(max_value(M))
-
-row, col = linear_sum_assignment(M, maximize=True)
-'''print(f"Coordinates: {list(zip(row, col))}")
-print(f"Max sum: {M[row, col].sum()}")
-'''
+print(calc_max_value(M))
