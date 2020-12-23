9bcd71f2762a9ffb36d3ca0c63ba271018078dc7
Velcorn <Velcorn@users.noreply.github.com>
2020-12-23 13:13:51 +0100

    Retarded task...

--- Sheet 4/task_4.1.py
+++ Sheet 4/task_4.1.py
@@ -1,20 +1,5 @@
 def in_range(A, a, b):
-	count = process_array(A)
-	num = 0
-	for i in range(a, b+1):
-		if count[i]:
-			num += count[i]
-	return num
-
-
-def process_array(A):
-	count = {}
-	for n in A:
-		if n in count:
-			count[n] += 1
-		else:
-			count[n] = 1
-	return count
+	return len([n for n in A if a <= n <= b])
 
 
 A = [1, 1, 2, 3, 5, 8, 9, 4]
