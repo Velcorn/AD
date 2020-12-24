0631f64a417cc4af8fbec5ea983ed6daca46a115
Velcorn <Velcorn@users.noreply.github.com>
2020-12-24 14:50:16 +0100

    Closer, I guess...

--- Sheet 4/task_4.1.py
+++ Sheet 4/task_4.1.py
@@ -3,4 +3,20 @@ def in_range(A, a, b):
 
 
 A = [1, 1, 2, 3, 5, 8, 9, 4]
-print(in_range(A, 1, 5))
+# print(in_range(A, 1, 5))
+
+
+def process_array(A):
+	count = {}
+	k = 0
+	for n in A:
+		if n in count:
+			count[n] += 1
+		else:
+			count[n] = 1
+		if n > k:
+			k = n
+	return count
+
+
+print(process_array(A))
