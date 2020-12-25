0c4e4f249a64e5271c6266d3f9d0f5cfe4d55bc7
velcorn <iwishyouperish@gmail.com>
2020-12-25 16:04:53 +0100

    Nope, this is better :P

--- Sheet 4/task_4.1.py
+++ Sheet 4/task_4.1.py
@@ -1,6 +1,5 @@
 def in_range(A, a, b):
 	count, k = process_array(A)
-	print(count, k)
 	if b > k:
 		b = k
 	if a <= 0:
@@ -10,15 +9,21 @@ def in_range(A, a, b):
 
 
 def process_array(A):
+	count = {}
 	k = 0
 	for n in A:
+		if n in count:
+			count[n] += 1
+		else:
+			count[n] = 1
 		if n > k:
 			k = n
-	count = [[0]*len(A)]*k
-	for i in range(len(A)-1):
-		for j in range(k-1):
-			count[i][j] = 0 if i == 0 else count[i-1][j]
-		count[i][A[i]] += 1
+	for i in range(k, -1, -1):
+		for j in range(-1, i):
+			if i not in count:
+				count[i] = 0
+			if j in count:
+				count[i] += count[j]
 	return count, k
 
 
