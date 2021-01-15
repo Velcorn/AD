7963c4733cf013995dc7f9f22bdd1b92b84daa93
Velcorn <Velcorn@users.noreply.github.com>
2021-01-15 11:18:55 +0100

    Use array instead of dict

--- Sheet 4/task_4.1.py
+++ Sheet 4/task_4.1.py
@@ -1,34 +1,31 @@
-def in_range(A, a, b):
-	count, k = process_array(A)
+def in_range(C, k, a, b):
 	if b > k:
 		b = k
 	if a <= 0:
-		return count[b]
+		return C[b]
 	else:
-		return count[b] - count[a-1]
+		return C[b] - C[a-1]
 
 
 def process_array(A):
-	count = {}
+	# Get k.
 	k = 0
 	for n in A:
-		if n in count:
-			count[n] += 1
-		else:
-			count[n] = 1
 		if n > k:
 			k = n
-	count[-1] = 0
-	for i in range(k+1):
-		if i in count:
-			count[i] += count[i-1]
-		else:
-			count[i] = count[i-1]
+	# Get count of each array element and largest array element.
+	count = [0]*(k+1)
+	for n in A:
+		count[n] += 1
+	# Get count of elements from 0 to n for every element n.
+	for i in range(1, k+1):
+		count[i] += count[i-1]
 	return count, k
 
 
 A = [1, 1, 2, 3, 5, 8, 9, 4]
+C, k = process_array(A)
 print(process_array(A))
-print(in_range(A, 1, 10))
-print(in_range(A, 0, 9))
-print(in_range(A, 1, 9))
+print(in_range(C, k, 1, 10))
+print(in_range(C, k, 0, 9))
+print(in_range(C, k, 1, 9))
