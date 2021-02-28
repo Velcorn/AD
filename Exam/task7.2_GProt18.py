5455e69455fd87c3a6838b4fd2f9643fc295487c
Velcorn <Velcorn@users.noreply.github.com>
2021-02-28 15:11:51 +0100

    Refactor

--- Exam/task7.2_GProt18.py
+++ Exam/task7.2_GProt18.py
@@ -1,4 +1,4 @@
-def lic(A):
+def longest_ascending_subsequence(A):
     l = [1 for x in range(len(A))]
     for i in range(1, len(A)):
         if A[i-1] < A[i]:
@@ -8,5 +8,5 @@ def lic(A):
 
 A = [1, 2, 3, 4, 5, 4, 3, 2, 1]
 print(A)
-print(lic(A))
-print(lic(A[::-1]))
+print(longest_ascending_subsequence(A))
+print(longest_ascending_subsequence(A[::-1]))
