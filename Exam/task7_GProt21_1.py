870c276af0f6ba3d5a234f477b7fb22173191a97
velcorn <iwishyouperish@gmail.com>
2021-03-01 23:02:32 +0100

    Minor improvements

--- Exam/task7_GProt21_1.py
+++ Exam/task7_GProt21_1.py
@@ -1,13 +1,11 @@
 def max_profit(W, n):
     G = [0 for x in range(n+1)]
+    G[0] = W[0]
+    G[1] = max(W[0], W[1])
     for i in range(0, n+1):
-        if i == 0:
-            G[i] = W[i]
-        elif i == 1:
-            G[i] = max(W[i], W[i+1])
-        else:
-            G[i] = max(G[i-1], W[i] + G[i-2])
-    return G, G[n]
+        G[i] = max(G[i-1], W[i] + G[i-2])
+    print(G)
+    return G[n]
 
 
 W = [2, 3, 4, 2, 4, 6, 2]
