f9275fbc62a23a9c203c561cedd540ed54db056f
Velcorn <Velcorn@users.noreply.github.com>
2021-03-14 12:51:40 +0100

    Refactor

--- Exam/task7.2_GProt18.py
+++ Exam/task7.2_GProt18.py
@@ -1,13 +1,15 @@
-def longest_ascending_subsequence(A):
-    l = [1 for x in range(len(A))]
-    for i in range(1, len(A)):
-        if A[i-1] < A[i]:
-            l[i] = l[i-1] + 1
-    return l, max(l)
+def lis(A):
+    n = len(A)
+    l = [1 for x in range(n)]
+    for i in range(n):
+        for j in range(i):
+            if A[i] > A[j]:
+                l[i] = max(l[i], l[j] + 1)
+    return max(l)
 
 
-A = [1, 3, 5, 7, 9, 1, 2, 3, 4]
+A = [1, 3, 1, 7, 1, 1, 9, 3, 4]
 print(A)
-print(longest_ascending_subsequence(A))
+print(lis(A))
 print(A[::-1])
-print(longest_ascending_subsequence(A[::-1]))
+print(lis(A[::-1]))
