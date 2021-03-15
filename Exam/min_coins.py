d65e425662fbcdb5ba4048300bb147796984a8ba
Velcorn <Velcorn@users.noreply.github.com>
2021-03-15 20:50:46 +0100

    Refactor me, daddy uwu

--- Exam/min_coins.py
+++ Exam/min_coins.py
@@ -1,18 +1,36 @@
 def min_coins(C, V):
     n = len(C)
+    table = [[0 for x in range(V+1)] for y in range(n+1)]
+    table[0] = [x for x in range(V+1)]
+    for i in range(1, n+1):
+        table[i][0] = 0
+        for j in range(V+1):
+            if C[i-1] > j:
+                table[i][j] = table[i-1][j]
+            else:
+                table[i][j] = min(table[i-1][j], table[i][j-C[i-1]] + 1)
+    for i in range(n+1):
+        if i == 0:
+            print(" ", table[i])
+        else:
+            print(C[i-1], table[i])
+    return f"Min number of coins: {table[n][V]}"
+
+
+def min_coins_gfg(C, V):
+    n = len(C)
     table = [1000 for x in range(V + 1)]
     table[0] = 0
     for i in range(V + 1):
         for j in range(n):
             if C[j] <= i:
-                x = table[i - C[j]]
-                if x + 1 < table[i]:
-                    table[i] = x + 1
+                table[i] = min(table[i], table[i - C[j]] + 1)
     if table[V] == 1000:
         return -1
     return table, table[V]
 
 
-C = [9, 6, 5, 1]
+C = [1, 5, 6, 9]
 V = 11
 print(min_coins(C, V))
+# print(min_coins_gfg(C, V))
--- Exam/task6_GProt19_1.py
+++ Exam/task6_GProt19_1.py
@@ -1,12 +1,12 @@
 def binomial(n, k):
-    b = [[0 for x in range(k+1)] for x in range(n+1)]
+    table = [[0 for x in range(k+1)] for x in range(n+1)]
     for i in range(n+1):
         for j in range(k+1):
             if j == 0 or j == i:
-                b[i][j] = 1
+                table[i][j] = 1
             else:
-                b[i][j] = b[i-1][j-1] + b[i-1][j]
-    return b[n][k]
+                table[i][j] = table[i-1][j-1] + table[i-1][j]
+    return table, table[n][k]
 
 
 print(binomial(5, 2))
